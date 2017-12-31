b7938853c0ad4a0fda999b3148060f4de3f1e318
John Musca <warlockjm@gmail.com>
2017-12-31 16:13:16 +1100

    Addition of proptypes and validation

--- modules/common/Body/Body.js
+++ modules/common/Body/Body.js
@@ -1,13 +1,22 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 class Body extends React.Component {
 	render() {
 		return (
 			<div>
-				<p>This is some body text</p>
+				<p>{this.props.content}</p>
 			</div>
 		)
 	}
+}
+
+Body.defaultProps = {
+	content: 'Defualt body content'
+}
+
+Body.propTypes = {
+	content: PropTypes.string
 }
 
 export default Body;

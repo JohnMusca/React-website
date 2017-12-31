cf626575fb985d488f87b39440c7f3973f522f10
John Musca <warlockjm@gmail.com>
2017-12-31 16:03:21 +1100

    Corrected issue with footer component

--- modules/pages/Home/Home.js
+++ modules/pages/Home/Home.js
@@ -6,25 +6,13 @@ import Footer from '../../common/Footer/Footer'
 class Home extends React.Component {
   render() {
     return (
-			<div>
-				test
+			<div> 
+				<Header />
+				<Body />
+				<Footer />
 			</div>
 		)
   }
 }
 
 export default Home;
-/*
-class Home extends React.Component {
-	render() {
-		return (
-			<div>
-				<Header />
-				<Body />
-				<Footer />
-			</div>
-		)
-	}
-}
-
-export default Home;*/

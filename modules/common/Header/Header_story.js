e90328f835dbc96c9b77f96900676199e5f3b7eb
John Musca <warlockjm@gmail.com>
2018-01-01 08:43:37 +1100

    Addition of storybook. Still some issues but it's serving now

--- modules/common/Header/Header_story.js
+++ modules/common/Header/Header_story.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import Header from './Header'
 
-storiesOf('Button', module)
-  .add('with text', () => (
-    <button onClick={action('clicked')}>Hello Button</button>
-  ))
-  .add('with some emoji', () => (
-    <button onClick={action('clicked')}>? ? ? ?</button>
-  ));
+storiesOf('Header', module)
+  .add('with no default text', () => (
+  	<Header />
+	));

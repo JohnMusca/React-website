56d43d221a0c08863ceb3ef6199a7362f5006195
John Musca <warlockjm@gmail.com>
2017-12-31 14:24:46 +1100

    Fixed url hash

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, hashHistory } from 'react-router'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import App from './modules/App'
 import About from './modules/About'
 import Repos from './modules/Repos'
 
 render((
-  <Router history={hashHistory}>
-    <Route path="/" component={App}/>
+	<Router history={browserHistory}>    
+		<Route path="/" component={App}/>
     <Route path="/repos" component={Repos}/>
     <Route path="/about" component={About}/>
   </Router>

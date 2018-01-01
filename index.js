c856e27b3880b1d40bd590155b4d82ff7bd7f629
John Musca <warlockjm@gmail.com>
2018-01-01 13:27:56 +1100

    Refactored to use react-router-dom

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, browserHistory, IndexRoute } from 'react-router'
-
-import Home from './modules/pages/Home/Home'
-import OurTraining from './modules/pages/OurTraining/OurTraining'
-import AboutTheClass from './modules/pages/AboutTheClass/AboutTheClass'
-import Contact from './modules/pages/Contact/Contact'
+import { BrowserRouter } from 'react-router-dom'
+import App from './modules/common/App/App';
 
 render((
-	<Router history={browserHistory}>    
-		<Route path="/" component={Home}/>
-    <Route path="/our_training" component={OurTraining}/>
-    <Route path="/about_the_class" component={AboutTheClass}/>
-    <Route path="/contact" component={Contact}/>
-  </Router>
-), document.getElementById('app'))
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+), document.getElementById('root'));
---
+++ modules/common/Routes/Routes.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import Home from '../../pages/Home/Home'
+import OurTraining from '../../pages/OurTraining/OurTraining'
+import AboutTheClass from '../../pages/AboutTheClass/AboutTheClass'
+import Contact from '../../pages/Contact/Contact'
+
+class Main extends React.Component {
+	render() {
+		return (
+				<div>   
+					<Route exact path="/" component={Home}/>
+    			<Route path="/our_training" component={OurTraining}/>
+    			<Route path="/about_the_class" component={AboutTheClass}/>
+    			<Route path="/contact" component={Contact}/>
+				</div>
+		)
+	}
+}
+
+export default Main;

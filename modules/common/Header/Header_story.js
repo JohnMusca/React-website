f0932b319a11150ae9ab36f44f783fd743bbfe89
John Musca <warlockjm@gmail.com>
2018-01-01 13:42:35 +1100

    Corrected issue with browserrouter in storybook file

--- modules/common/Header/Header_story.js
+++ modules/common/Header/Header_story.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import { BrowserRouter } from 'react-router-dom'
 import Header from './Header'
 
 storiesOf('Header', module)
   .add('with no default text', () => (
-  	<Header />
+		<BrowserRouter>
+  		<Header />
+		</BrowserRouter>
 	));

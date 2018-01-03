f2548f86a913be9a7bcbee05feef3f6eea93312c
John Musca <warlockjm@gmail.com>
2018-01-03 14:33:33 +1100

    Addition of about us content

--- modules/pages/AboutTheClass/AboutTheClass.js
+++ modules/pages/AboutTheClass/AboutTheClass.js
@@ -4,7 +4,18 @@ import Body from '../../common/Body/Body'
 import Footer from '../../common/Footer/Footer'
 
 //Content passed into body component
-var content = "<p>About the class content</p>";
+var content = "<p>The Buk Sing Choy Lay Fut Kung Fu School services the Northern and Western suburbs and is located at 74 Melville Road, Brunswick West, which has immediate access to the suburbs of; Strathmore, Essendon, Pascoe Vale, Moonee Ponds, Ascot Vale, Maribyrnong, Kensington, Flemington, North Melbourne, Brunswick and Coburg.</p>" +
+              "<p>Training times are Tuesday and Thursday - 6:30pm to 8:30pm.</p>" +
+              "<p>Classes cater to all age groups, genders and backgrounds.</p>" +
+              "<p>Small class sizes provide a high degree of pastoral care and teaching is conducted in a non violent non threatening manner with particular regard to humility and respect.</p>" +
+              "<p>Dress consists of: gym shoes, T Shirt (black preferable) and long Black Martial Arts pants (shorts are acceptable initially).</p>" +
+              "<p>Cost to be instructed in Buk Sing Choy Lay Fut Kung Fu comprises:" +
+              "<ul>" +
+                "<li>Joining fee which includes cost of Buk Sing Choy Lay Fut training T Shirt;</li>" +
+                "<li>Monthly instruction fee;</li>" +
+                "<li>No grading or other ongoing fees.</li>" +
+              "</ul></p>" +
+              "<p>Sifu Gary Mallia welcomes you to your Free Introductory Class and looks forward to assisting with what is considered a significant life changing experience.</p>";
 
 /**
 * About the class page component used to render the page.
@@ -17,8 +28,8 @@ class AboutTheClass extends React.Component {
   render () {
     return (
       <div>
-        <div heading="About the class" class="hero-image-about"></div>
-        <Body content={content} />
+        <div  class="hero-image-about"></div>
+        <Body heading="About the class" content={content} />
         <Footer />
       </div>
     )

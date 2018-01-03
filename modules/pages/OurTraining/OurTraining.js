68818221b97a6c6322b0054e9b4b9830e957ffdc
John Musca <warlockjm@gmail.com>
2018-01-03 13:04:20 +1100

    Created base training content

--- modules/pages/OurTraining/OurTraining.js
+++ modules/pages/OurTraining/OurTraining.js
@@ -4,7 +4,22 @@ import Body from '../../common/Body/Body'
 import Footer from '../../common/Footer/Footer'
 
 //Content passed into body component.
-var content="<p>Our training content</p>";
+var content="<p>Our Training comprises a variety and combination of:" +
+            "<ul>" +
+              "<li>Practical instruction of Buk Sing philosophies and applications;</li>" + 
+              "<li>Strong focus on combat techniques and tactics;</li>" + 
+              "<li>Partner drills / Pad work;</li>" + 
+              "<li>Sparring;</li>" + 
+              "<li>Kung Fu Forms including use of weapons;</li>" + 
+              "<li>Wooden dummy training.</li>" + 
+            "</ul></p>" +
+            "<p>Sifu Gary Mallia believes that effort equals reward and that the results from his instruction, will improve:" + 
+            "<ul>" + 
+              "<li>Self Confidence and Self Esteem;</li>" + 
+              "<li>Ability to comprehensively and effectively defend against a variety of opponents;</li>" + 
+              "<li>Physical Fitness and Well Being;</li>" + 
+              "<li>Flexibility, Balance and Co-Ordination.</li>" + 
+            "</ul></p>";
 
 /**
 * Our Training page component used to render the page.

891e481cfad4ba7b52fa6fd37fbb960418e1b2dc
John Musca <warlockjm@gmail.com>
2018-01-03 16:54:27 +1100

    Addition of additional image in content

--- modules/pages/AboutTheClass/AboutTheClass.js
+++ modules/pages/AboutTheClass/AboutTheClass.js
@@ -6,9 +6,9 @@ import Footer from '../../common/Footer/Footer'
 //Content passed into body component
 var content = "<p>The Buk Sing Choy Lay Fut Kung Fu School services the Northern and Western suburbs and is located at 74 Melville Road, Brunswick West, which has immediate access to the suburbs of; Strathmore, Essendon, Pascoe Vale, Moonee Ponds, Ascot Vale, Maribyrnong, Kensington, Flemington, North Melbourne, Brunswick and Coburg.</p>" +
               "<p>Training times are Tuesday and Thursday - 6:30pm to 8:30pm.</p>" +
-              "<p><div style='float:left'>Classes cater to all age groups, genders and backgrounds."+ 
-              "<br />Small class sizes provide a high degree of pastoral care and teaching is conducted in a non violent non threatening manner with particular regard to humility and respect.</div>" +
-              "<div style='float:right; padding-left:10px;'><img src='/public/img/mal_small.jpg' /></div></p>" +
+              "<p>Classes cater to all age groups, genders and backgrounds.</p>"+ 
+              "<p><img src='/public/img/mal_small.jpg' /></p>" +
+              "<p>Small class sizes provide a high degree of pastoral care and teaching is conducted in a non violent non threatening manner with particular regard to humility and respect.</p>" +
               "<p>Dress consists of: gym shoes, T Shirt (black preferable) and long Black Martial Arts pants (shorts are acceptable initially).</p>" +
               "<p>Cost to be instructed in Buk Sing Choy Lay Fut Kung Fu comprises:" +
               "<ul>" +
@@ -16,6 +16,7 @@ var content = "<p>The Buk Sing Choy Lay Fut Kung Fu School services the Northern
                 "<li>Monthly instruction fee;</li>" +
                 "<li>No grading or other ongoing fees.</li>" +
               "</ul></p>" +
+              "<p><img src='/public/img/john_bag_small.jpg' /></p>" +
               "<p>Sifu Gary Mallia welcomes you to your Free Introductory Class and looks forward to assisting with what is considered a significant life changing experience.</p>";
 
 /**

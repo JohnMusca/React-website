4cb103b7fae42978405b9b669e6e45571945c591
John Musca <warlockjm@gmail.com>
2018-01-04 18:56:45 +1100

    Content change to about us

--- src/modules/config/content/about_us.js
+++ src/modules/config/content/about_us.js
@@ -1,5 +1,6 @@
 var JSONContent = {
-  "about_us": ["<p>The Buk Sing Choy Lay Fut Kung Fu School services the Northern and Western suburbs and is located at 74 Melville Road, Brunswick West, which has immediate access to the suburbs of; Strathmore, Essendon, Pascoe Vale, Moonee Ponds, Ascot Vale, Maribyrnong, Kensington, Flemington, North Melbourne, Brunswick and Coburg.</p>",
+  "about_us": ["<p>The Buk Sing Choy Lay Fut Kung Fu is in close proximity to the Northern and Western suburbs and is located at <a href='https://maps.google.com/maps?ll=-37.762813,144.944255&z=14&t=m&hl=en&gl=US&mapclient=embed&q=74%20Melville%20Rd%20Brunswick%20West%20VIC%203055' target='_blank'>74 Melville Road, Brunswick West</a>, which has immediate access to the suburbs of; Strathmore, Essendon, Pascoe Vale, Moonee Ponds, Ascot Vale, Maribyrnong, Kensington, Flemington, North Melbourne, Brunswick and Coburg. </p>",
+              "<p>We have students from all over Melbourne, from the Mornington peninsula to Greensborough.</p>",
               "<p>Training times are Tuesday and Thursday - 6:30pm to 8:30pm.</p>",
               "<p>Classes cater to all age groups, genders and backgrounds.</p>",
               "<p><img src='/img/mal_small.jpg' /></p>",

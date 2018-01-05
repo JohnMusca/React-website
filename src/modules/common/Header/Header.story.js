d4fde763b3187e1b34371a36f1a2c0aa05004bc4
John Musca <warlockjm@gmail.com>
2018-01-05 11:24:58 +1100

    Fixed storybook files

--- src/modules/common/Header/Header.story.js
+++ src/modules/common/Header/Header.story.js
@@ -2,7 +2,15 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { BrowserRouter } from 'react-router-dom'
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs/react';
+
 import Header from './Header'
+
+const stories = storiesOf('Storybook Knobs', module);
+
+// Add the `withKnobs` decorator to add knobs support to your stories.
+// You can also configure `withKnobs` as a global decorator.
+stories.addDecorator(withKnobs);
 
 storiesOf('Header', module)
   .add('Default Header', () => (
